a0ab46711ce979e701891bb57528152b4fbfc69e
Patrick Vollebregt <patrick@crosscode.nl>
2020-10-09 17:34:02 +0200

    Added more tests

--- include/simple_telemetry/telemetry.h
+++ include/simple_telemetry/telemetry.h
@@ -7,61 +7,83 @@ namespace simple_telemetry {
 
     template <typename Tvalue, typename Tmetadata, typename Texporter>
     struct data_block  {
-        std::shared_ptr<Texporter> exporter_;
-        Tmetadata metadata_;
-        Tvalue value_;
+        using value_type = Tvalue;
+        using metadata_type = Tmetadata;
+        using exporter_type = Texporter;
+        using exporter_shared_ptr_type = std::shared_ptr<exporter_type>;
+        exporter_shared_ptr_type exporter_;
+        metadata_type metadata_;
+        value_type value_;
     };
 
     template <typename Tvalue, typename Tmetadata, typename Texporter>
-    class atomic_counter {
-        data_block<std::atomic<Tvalue>,Tmetadata,Texporter> data_;
+    class atomic_bidirectional_counter {
+    public:
+        using value_type = Tvalue;
+        using metadata_type = Tmetadata;
+        using exporter_type = Texporter;
+    private:
+        data_block<std::atomic<value_type>,metadata_type,exporter_type> data_;
     public:
         template <typename ...Args>
-        explicit atomic_counter(Args ...args) : data_{std::forward<Args>(args)...} {}
+        explicit atomic_bidirectional_counter(Args ...args) : data_{std::forward<Args>(args)...} {}
 
-        void incr(Tvalue amount=1, std::memory_order mem_order = std::memory_order::memory_order_seq_cst) {
-            auto new_value = data_.value_.fetch_add(amount, mem_order) + amount;
+        void add(value_type amount= 1, std::memory_order mem_order = std::memory_order::memory_order_seq_cst) {
+            value_type new_value = value_type{data_.value_.fetch_add(amount, mem_order)} + amount;
             data_.exporter_->on_data_changed(new_value, data_.metadata_);
         }
 
-        void decr(Tvalue amount=1, std::memory_order mem_order = std::memory_order::memory_order_seq_cst) {
-            auto new_value = data_.value_.fetch_sub(amount, mem_order) - amount;
+        void sub(value_type amount= 1, std::memory_order mem_order = std::memory_order::memory_order_seq_cst) {
+            value_type new_value = value_type{data_.value_.fetch_sub(amount, mem_order)} - amount;
             data_.exporter_->on_data_changed(new_value, data_.metadata_);
         }
 
-        Tvalue value(std::memory_order mem_order = std::memory_order::memory_order_seq_cst) {
+        value_type value(std::memory_order mem_order = std::memory_order::memory_order_seq_cst) {
             return data_.value_.load(mem_order);
+        }
+    };
+
+    template <typename Tvalue, typename Tmetadata, typename Texporter>
+    class atomic_monotonic_counter {
+    public:
+        using value_type = Tvalue;
+        using metadata_type = Tmetadata;
+        using exporter_type = Texporter;
+    private:
+        atomic_bidirectional_counter<value_type,metadata_type,exporter_type> counter_;
+    public:
+        template <typename ...Args>
+        explicit atomic_monotonic_counter(Args ...args) : counter_{std::forward<Args>(args)...} {}
+
+        void add(std::memory_order mem_order = std::memory_order::memory_order_seq_cst) {
+            counter_.add(1, mem_order);
+        }
+
+        value_type value(std::memory_order mem_order = std::memory_order::memory_order_seq_cst) {
+            return counter_.value(mem_order);
         }
     };
 
     template <typename Tmetadata, typename Texporter>
     class telemetry {
+    public:
+        using metadata_type = Tmetadata;
+        using exporter_type = Texporter;
+        using exporter_shared_ptr_type = std::shared_ptr<exporter_type>;
     private:
-        std::shared_ptr<Texporter> impl_;
-
-        template<typename Tvalue>
-        atomic_counter<Tvalue,Tmetadata,Texporter> create_atomic_counter(Tmetadata metadata = {}, Tvalue value = 0) {
-            return atomic_counter<Tvalue,Tmetadata,Texporter>{impl_, std::move(metadata), value};
-        }
-
+        exporter_shared_ptr_type impl_;
     public:
         template <typename ...Args>
-        explicit telemetry(Args ...args) : impl_{std::make_shared<Texporter>(std::forward<Args>(args)...)} {}
+        explicit telemetry(Args ...args) : impl_{std::make_shared<exporter_type>(std::forward<Args>(args)...)} {}
 
-        auto create_uint64_atomic_counter(Tmetadata metadata = {}, uint64_t value = 0) {
-            return create_atomic_counter(std::move(metadata),value);
+        template<typename Tvalue>
+        atomic_bidirectional_counter<Tvalue,metadata_type,exporter_type> create_atomic_bidirectional_counter(Tmetadata metadata = {}, Tvalue value = 0) {
+            return atomic_bidirectional_counter<Tvalue,Tmetadata,Texporter>{impl_, std::move(metadata), value};
         }
 
-        auto create_int64_atomic_counter(Tmetadata metadata = {}, int64_t value = 0) {
-            return create_atomic_counter(std::move(metadata),value);
-        }
-
-        auto create_uint32_atomic_counter(Tmetadata metadata = {}, uint32_t value = 0) {
-            return create_atomic_counter(std::move(metadata),value);
-        }
-
-        auto create_int32_atomic_counter(Tmetadata metadata = {}, int32_t value = 0) {
-            return create_atomic_counter(std::move(metadata),value);
+        template<typename Tvalue>
+        atomic_monotonic_counter<Tvalue,metadata_type,exporter_type> create_atomic_monotonic_counter(Tmetadata metadata = {}, Tvalue value = 0) {
+            return atomic_monotonic_counter<Tvalue,Tmetadata,Texporter>{impl_, std::move(metadata), value};
         }
 
     };
--- tests/telemetry_tests.cpp
+++ tests/telemetry_tests.cpp
@@ -26,19 +26,39 @@ TEST_SUITE("telemetry") {
     TEST_CASE("Can create telemetry") {
         std::stringstream ss;
         simple_telemetry::telemetry<metadata, exporter> telemetry{exporter{&ss}};
+        SUBCASE("Can create int16_t counter and is initialized with 0") {
+            auto counter = telemetry.create_atomic_bidirectional_counter<int16_t>({"test_overflow"});
+            static_assert(std::is_same_v<decltype(counter)::value_type,std::int16_t>,"value_type should be the same  type as int64_t");
+            static_assert(std::is_same_v<decltype(counter)::metadata_type,metadata>,"metadata_type should be the same type as metadata");
+            static_assert(std::is_same_v<decltype(counter)::exporter_type,exporter>,"exporter_type should be the same type as exporter");
+            SUBCASE("Can decrease with std::numeric_limits<int16_t>::min() and value become std::numeric_limits<int16_t>::min()") {
+                counter.add(std::numeric_limits<int16_t>::min());
+                REQUIRE(std::numeric_limits<int16_t>::min()==counter.value());
+                SUBCASE("Can decrease one more, and value becomes std::numeric_limits<int16_t>::max()") {
+                    counter.sub(1);
+                    REQUIRE(std::numeric_limits<int16_t>::max() == counter.value());
+                    SUBCASE("Serialized correctly\\n") {
+                        REQUIRE(ss.str()=="test_overflow -32768\ntest_overflow 32767\n");
+                    }
+                }
+            }
+        }
         SUBCASE("Can create uint64 counter and is initialized with 0")
         {
-            auto counter = telemetry.create_uint64_atomic_counter({"test"});
+            auto counter = telemetry.create_atomic_bidirectional_counter<uint64_t>({"test"});
+            static_assert(std::is_same_v<decltype(counter)::value_type,std::uint64_t>,"value_type should be the same  type as uint64_t");
+            static_assert(std::is_same_v<decltype(counter)::metadata_type,metadata>,"metadata_type should be the same type as metadata");
+            static_assert(std::is_same_v<decltype(counter)::exporter_type,exporter>,"exporter_type should be the same type as exporter");
             REQUIRE(counter.value()==0);
             SUBCASE("Increment default increments counter with 1") {
-                counter.incr();
+                counter.add();
                 REQUIRE(counter.value()==1);
                 SUBCASE("Serialized to test 1\\n") {
                     REQUIRE(ss.str()=="test 1\n");
                 }
             }
             SUBCASE("Increment with 2 increments counter with 2") {
-                counter.incr(2);
+                counter.add(2);
                 REQUIRE(counter.value()==2);
                 SUBCASE("Serialized to test 2\\n") {
                     REQUIRE(ss.str()=="test 2\n");
@@ -47,132 +67,17 @@ TEST_SUITE("telemetry") {
         }
         SUBCASE("Can create uint64 counter with default value 10 and is initialized with 10")
         {
-            auto counter = telemetry.create_uint64_atomic_counter({"test"},10);
+            auto counter = telemetry.create_atomic_bidirectional_counter<uint64_t>({"test"},10);
             REQUIRE(counter.value()==10);
             SUBCASE("Increment default increments counter with 1 to 11") {
-                counter.incr();
+                counter.add();
                 REQUIRE(counter.value()==11);
                 SUBCASE("Serialized to test 11\\n") {
                     REQUIRE(ss.str()=="test 11\n");
                 }
             }
             SUBCASE("Descrement default decrements counter with 1 to 9") {
-                counter.decr();
-                REQUIRE(counter.value()==9);
-                SUBCASE("Serialized to test 9\\n") {
-                    REQUIRE(ss.str()=="test 9\n");
-                }
-            }
-        }
-        SUBCASE("Can create int64 counter and is initialized with 0")
-        {
-            auto counter = telemetry.create_int64_atomic_counter({"test"});
-            REQUIRE(counter.value()==0);
-            SUBCASE("Increment default increments counter with 1") {
-                counter.incr();
-                REQUIRE(counter.value()==1);
-                SUBCASE("Serialized to test 1\\n") {
-                    REQUIRE(ss.str()=="test 1\n");
-                }
-            }
-            SUBCASE("Increment with 2 increments counter with 2") {
-                counter.incr(2);
-                REQUIRE(counter.value()==2);
-                SUBCASE("Serialized to test 2\\n") {
-                    REQUIRE(ss.str()=="test 2\n");
-                }
-            }
-        }
-        SUBCASE("Can create int64 counter with default value 10 and is initialized with 10")
-        {
-            auto counter = telemetry.create_int64_atomic_counter({"test"},10);
-            REQUIRE(counter.value()==10);
-            SUBCASE("Increment default increments counter with 1 to 11") {
-                counter.incr();
-                REQUIRE(counter.value()==11);
-                SUBCASE("Serialized to test 11\\n") {
-                    REQUIRE(ss.str()=="test 11\n");
-                }
-            }
-            SUBCASE("Decrement default decrements counter with 1 to 9") {
-                counter.decr();
-                REQUIRE(counter.value()==9);
-                SUBCASE("Serialized to test 9\\n") {
-                    REQUIRE(ss.str()=="test 9\n");
-                }
-            }
-        }
-
-        SUBCASE("Can create uint32 counter and is initialized with 0")
-        {
-            auto counter = telemetry.create_uint32_atomic_counter({"test"});
-            REQUIRE(counter.value()==0);
-            SUBCASE("Increment default increments counter with 1") {
-                counter.incr();
-                REQUIRE(counter.value()==1);
-                SUBCASE("Serialized to test 1\\n") {
-                    REQUIRE(ss.str()=="test 1\n");
-                }
-            }
-            SUBCASE("Increment with 2 increments counter with 2") {
-                counter.incr(2);
-                REQUIRE(counter.value()==2);
-                SUBCASE("Serialized to test 2\\n") {
-                    REQUIRE(ss.str()=="test 2\n");
-                }
-            }
-        }
-        SUBCASE("Can create uint32 counter with default value 10 and is initialized with 10")
-        {
-            auto counter = telemetry.create_uint32_atomic_counter({"test"},10);
-            REQUIRE(counter.value()==10);
-            SUBCASE("Increment default increments counter with 1 to 11") {
-                counter.incr();
-                REQUIRE(counter.value()==11);
-                SUBCASE("Serialized to test 11\\n") {
-                    REQUIRE(ss.str()=="test 11\n");
-                }
-            }
-            SUBCASE("Descrement default decrements counter with 1 to 9") {
-                counter.decr();
-                REQUIRE(counter.value()==9);
-                SUBCASE("Serialized to test 9\\n") {
-                    REQUIRE(ss.str()=="test 9\n");
-                }
-            }
-        }
-        SUBCASE("Can create int32 counter and is initialized with 0")
-        {
-            auto counter = telemetry.create_int32_atomic_counter({"test"});
-            REQUIRE(counter.value()==0);
-            SUBCASE("Increment default increments counter with 1") {
-                counter.incr();
-                REQUIRE(counter.value()==1);
-                SUBCASE("Serialized to test 1\\n") {
-                    REQUIRE(ss.str()=="test 1\n");
-                }
-            }
-            SUBCASE("Increment with 2 increments counter with 2") {
-                counter.incr(2);
-                REQUIRE(counter.value()==2);
-                SUBCASE("Serialized to test 2\\n") {
-                    REQUIRE(ss.str()=="test 2\n");
-                }
-            }
-        }
-        SUBCASE("Can create int32 counter with default value 10 and is initialized with 10")
-        {
-            auto counter = telemetry.create_int32_atomic_counter({"test"},10);
-            REQUIRE(counter.value()==10);
-            SUBCASE("Increment default increments counter with 1 to 11") {
-                counter.incr();
-                REQUIRE(counter.value()==11);
-                SUBCASE("Serialized to test 11\\n") {
-                    REQUIRE(ss.str()=="test 11\n");
-                }
-            }
-            SUBCASE("Decrement default decrements counter with 1 to 9") {
-                counter.decr();
+                counter.sub();
                 REQUIRE(counter.value()==9);
                 SUBCASE("Serialized to test 9\\n") {
                     REQUIRE(ss.str()=="test 9\n");
